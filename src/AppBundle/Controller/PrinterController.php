b87db978733bdbc47037d77fe1f5f5932508bff7
Manu974 <emmanuel.dijoux14@gmail.com>
2017-08-02 08:37:44 +0200

    ajout d'imprimante

--- src/AppBundle/Controller/PrinterController.php
+++ src/AppBundle/Controller/PrinterController.php
@@ -5,6 +5,9 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Imprimante;
+use AppBundle\Form\ImprimanteType;
 
 class PrinterController extends Controller
 {
@@ -24,6 +27,32 @@ class PrinterController extends Controller
         // replace this example code with whatever you need
         return $this->render('printer/index.html.twig', [
             "printers" => $printers,
+        ]);
+    }
+
+
+    /**
+     * @Route("/printer/add", name="addprinterpage")
+     */
+    public function addAction(Request $request)
+    {
+        $printer = new Imprimante();
+
+        $form = $this->get('form.factory')->create(ImprimanteType::class, $printer);
+
+       if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+          $em = $this->getDoctrine()->getManager();
+          $em->persist($printer);
+          $em->flush();
+
+          $request->getSession()->getFlashBag()->add('message', 'Imprimante bien enregistrée.');
+
+          return $this->redirectToRoute('printerpage');
+        }   
+
+        // replace this example code with whatever you need
+        return $this->render('printer/add.html.twig', [
+            "form" => $form->createView(),
         ]);
     }
 }
---
+++ src/AppBundle/Form/ImprimanteType.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class ImprimanteType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('marque', TextType::class)
+            ->add('modele', TextType::class)
+            ->add('localisation', TextType::class, [
+                'label' => "Où est installé l'imprimante ?",
+                ])
+            ->add('type', ChoiceType::class, [
+                 'choices'  => array(
+                        "Laser monochrome" => "Laser monochrome",
+                        "Laser couleur" => "Laser couleur",
+                        "Jet d'encre monochrome" => "Jet d'encre monochrome",
+                        "Jet d'encre couleur" => "Jet d'encre couleur",
+                    ),
+                ])
+            ->add('adresseip', TextType::class, [
+                'label' => 'Adresse Ip',
+                ])
+            ->add('dateachat', DateType::class, [
+                'label' => "Date de livraison",
+                ])
+            ->add('garantie', IntegerType::class, [
+                'label' => 'Années de garantie',
+                ])
+            ->add('save', SubmitType::class);
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Imprimante'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_imprimante';
+    }
+
+
+}
